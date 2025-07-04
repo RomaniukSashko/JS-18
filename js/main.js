56b7523fd62d261b353b2865994481c1e04e4a1a
RomaniukSashko <166243339+RomaniukSashko@users.noreply.github.com>
2025-07-04 17:02:52 +0300

    second

---
+++ js/main.js
@@ -0,0 +1,115 @@
+// Завдання 1
+console.log("Завдання 1");
+
+// В HTML є список категорій ul#categories:
+// * Тварини;
+// * Продукти;
+// * Технології.
+// Напиши скрипт, який виконає наступні операції:
+// * Порахує і виведе в консоль кількість категорій в ul#categories,
+// тобто елементів li.item. Вийде 'У списку 3 категорії.';
+// * Для кожного елемента li.item в списку ul#categories,
+// знайде і виведе в консоль текст заголовка елемента (тега h2)
+// і кількість елементів в категорії (всіх вкладених в нього елементів li).
+// Наприклад, для першої категорії вийде:
+// Категорія: Тварини
+// Кількість елементів: 4
+
+const firstLiRef = document.querySelectorAll("#categories > li");
+console.log(`У списку ${firstLiRef.length} категорії.`);
+
+const titleRef = document.querySelectorAll("#categories > li > h2");
+const secondLiRef = document.querySelectorAll("#categories > li > ul > li");
+console.log(secondLiRef);
+
+
+console.log(`Категорія: ${titleRef[0].textContent}; Кількість елементів: ${secondLiRef.length}`);
+console.log(`Категорія: ${titleRef[1].textContent}; Кількість елементів: ${secondLiRef.length}`);
+console.log(`Категорія: ${titleRef[2].textContent}; Кількість елементів: ${secondLiRef.length}`);
+
+
+
+
+
+
+// Завдання 2
+console.log("Завдання 2");
+
+// В HTML є пустий список ul#ingredients.
+// В JS є масив рядків ingredients.
+// Напиши скрипт, який для кожного елемента масиву ingredients створить окремий li,
+// після чого вставить всі li за одну операцію в список ul.ingredients.
+// Для створення DOM-вузлів використовуй document.createElement().
+
+const ingredients = [
+  "Картопля",
+  "Гриби",
+  "Часник",
+  "Помідори",
+  "Зелень",
+  "Приправи",
+];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const createLi = (item) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = `${item}`;
+  return itemEl;
+  // const itemEl1 = document.createElement("li");
+  // itemEl1.textContent = `${item1}`;
+  // const itemEl2 = document.createElement("li");
+  // itemEl2.textContent = `${item2}`;
+  // const itemEl3 = document.createElement("li");
+  // itemEl3.textContent = `${item3}`;
+  // const itemEl4 = document.createElement("li");
+  // itemEl4.textContent = `${item4}`;
+  // const itemEl5 = document.createElement("li");
+  // itemEl5.textContent = `${item5}`;
+  // const itemEl6 = document.createElement("li");
+  // itemEl6.textContent = `${item6}`;
+  // return itemEl1, itemEl2, itemEl3, itemEl4, itemEl5, itemEl6;
+};
+
+ingredients.forEach((ingredient) => {
+  ingredientsRef.appendChild(createLi(ingredients));
+});
+
+
+
+// Завдання 3
+console.log("Завдання 3");
+
+// Напиши скрипт для створення галереї зображень по масиву даних.
+// В HTML є список ul#gallery.
+// Використовуй масив об'єктів images для створення тегів img вкладених в li.
+// Для створення розмітки використовуй шаблонні рядки і insertAdjacentHTML().
+// Всі елементи галереї повинні додаватися в DOM за одну операцію вставки.
+// Додай мінімальне оформлення галереї флексбоксами або грід через css-класи.
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const listRef = document.querySelector("#gallery");
+listRef.style.listStyle = "none";
+
+images.forEach(({ url, alt }) => {
+  listRef.insertAdjacentHTML(
+    "beforeend",
+    `<li>
+        <img src="${url}" alt="${alt}" style="width: 500px; border-radius: 10%; border: solid 5px black;"></img>
+    </li>`
+  );
+});
